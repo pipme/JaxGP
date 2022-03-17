cfa17aac66d7f022ef9eec74a9d416d627c8d017
pipme <sjtulck@gmail.com>
2022-03-17 13:35:17 +0200

    Fix.

--- jaxgp/__init__.py
+++ jaxgp/__init__.py
@@ -8,7 +8,6 @@ config.update("jax_enable_x64", True)
 from jaxgp import gps, kernels, likelihoods, means
 from jaxgp.config import Config
 from jaxgp.gps import GPrior
-from jaxgp.jaxgp_version import version as __version__
 from jaxgp.parameters import copy_dict_structure, initialise
 from jaxgp.posteriors import construct_posterior
 from jaxgp.sgpr import SGPR
@@ -18,6 +17,7 @@ from jaxgp.types import Dataset
 __author__ = "Chengkun Li"
 __email__ = "sjtulck@gmail.com"
 __uri__ = "https://github.com/pipme/JaxGP"
+__copyright__ = "Copyright 2022, Machine and Human Intelligence Group, University of Helsinki"
 __license__ = "MIT"
 __description__ = "A Gaussian Process library in Jax"
 __version__ = "0.0.1"
